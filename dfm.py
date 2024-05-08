b87c6d6cf7eb0930cd296576a424f347af943c7d
Alex Langford <nirvashtzero@gmail.com>
2024-05-07 21:08:22 -0700

    Create dotfile manager tool from install script

--- dfm.py
+++ dfm.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 import typing
@@ -19,12 +20,11 @@ FILES = {
 }
 
 
-def main():
+def install(args):
     dotfiles_dir = os.path.dirname(os.path.realpath(__file__))
-    home_dir = os.path.expanduser("~")
 
     for file, file_config in FILES.items():
-        destination_dir = os.path.join(home_dir, file_config.relative_path)
+        destination_dir = os.path.join(args.dest_dir, file_config.relative_path)
         filename = file
         if file_config.relative_path == "":
             filename = "." + filename
@@ -42,6 +42,28 @@ def main():
             os.symlink(source, destination)
         except FileExistsError:
             pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description="dotfile management tool")
+    subparsers = parser.add_subparsers()
+
+    install_command_parser = subparsers.add_parser("install")
+    install_command_parser.add_argument(
+        "--dest-dir",
+        type=str,
+        nargs=1,
+        default=os.path.expanduser("~"),
+        required=False,
+        help="Destination directory to install dotfiles to (Defaults to homedir)",
+    )
+    install_command_parser.set_defaults(func=install)
+
+    args = parser.parse_args()
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
